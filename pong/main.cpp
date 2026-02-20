8340e49070665987d0cd6c0083d068cea6f50d5d
Giovanny Orozco Quintero <giovannyorozco4000@gmail.com>
2026-02-20 12:04:08 -0500

    Pong: Agregar colisiones en los bordes de la ventana

--- pong/main.cpp
+++ pong/main.cpp
@@ -15,6 +15,14 @@ class Ball {
     void Update() {
       x += speed_x;
       y += speed_y;
+
+      if (y + radius >= GetScreenHeight() || y - radius <= 0) {
+        speed_y *= -1;
+      }
+
+      if (x + radius >= GetScreenWidth() || x - radius <= 0) {
+        speed_x *= -1;
+      }
     }
 };
 
