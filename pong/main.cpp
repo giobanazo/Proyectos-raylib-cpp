4d9908996bcdcac45c9588c0a8f279dc9d3d99e6
Giovanny Orozco Quintero <giovannyorozco4000@gmail.com>
2026-02-21 10:08:07 -0500

    Pong: Darle movimiento a la paleta derecha cuando presiona las flechas arriba y abajo

--- pong/main.cpp
+++ pong/main.cpp
@@ -26,7 +26,36 @@ class Ball {
     }
 };
 
+class Paddle {
+  public:
+    float x, y, width, height;
+    int speed;
+
+    void Draw() {
+      DrawRectangle(x, y, width, height, WHITE); 
+    }
+
+    void Update() {
+      if (IsKeyDown(KEY_UP)) {
+        y -= speed;
+      }
+
+      if (IsKeyDown(KEY_DOWN)) {
+        y += speed;
+      }
+
+      if (y <= 0) {
+        y = 0;
+      }
+
+      if (y + height >= GetScreenHeight()) {
+        y = GetScreenHeight() - height;
+      }
+    }
+};
+
 Ball ball;
+Paddle player;
 
 int main() {
   const int screen_width = 1180;
@@ -41,16 +70,25 @@ int main() {
   ball.speed_x = 7;
   ball.speed_y = 7;
 
+  player.width = 25;
+  player.height = 120;
+  player.x = screen_width - player. width - 10;
+  player.y = screen_height / 2 - player.height / 2;
+  player.speed = 6;
+
   while(!WindowShouldClose()) {
+    ball.Update();
+    player.Update();
+
     BeginDrawing();
       ClearBackground(BLACK);
       DrawLine(screen_width / 2, 0, screen_width / 2, screen_height, WHITE);
+    
       ball.Draw();
-      ball.Update();
 
       // el 60 lo sacamos de la altura del rectangulo (120) dividido entre 2
       DrawRectangle(10, screen_height / 2 - 60 , 25, 120, WHITE);
-      DrawRectangle(screen_width - 35, screen_height / 2 - 60, 25, 120, WHITE);
+      player.Draw();
     EndDrawing();
   }
 
